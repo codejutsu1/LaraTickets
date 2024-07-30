6e690cb95df2c45f6dd623bc8b1d0268464ccfa7
codejutsu1 <codejutsu@protonmail.com>
2024-07-30 19:05:00 +0100

    able to create an admin

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # LaraTickets
 
 LaraTickets  is a support portal that enables users to log their complaints. 
+
+- To create an Admin
+
+```bash
+php artisan admin:create
+```
---
+++ app/Console/Commands/RegisterAdmin.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'admin:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Register an Admin to the application';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = $this->ask('What is the admin\'s name?');
+
+        $email = $this->ask('What is the admin\'s email?');
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->error('Invalid email format');
+            return 1; 
+        }
+
+        $password = $this->secret('What is the admin\'s password?');
+
+        $hashedPassword = Hash::make($password);
+
+        try {
+            User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => $hashedPassword,
+                'role_id' => 1,
+            ]);
+
+            $this->info('Admin created successfully.');
+        } catch (\Exception $e) {
+            $this->error('Error creating admin: ' . $e->getMessage());
+            return 1; 
+        }
+
+        return 0; 
+    }
+}
